03bc9487011e0fcab331f8dfbefb7d61b7dcc8fb
JuanC1476 <juancruzmouriat@gmail.com>
2025-02-08 11:14:13 -0300

    Base de datos guardando y cargando correctamente

--- main.php
+++ main.php
@@ -4,8 +4,15 @@ require_once "database/operaciones.php";
 require_once "database/conection.php";  
 require_once "class/Madre.php";     
 require_once "class/Repository.php";
+require_once "class/Vacuno.php";
 
 $repository = new Repository($conection);
+
+$madre = new Madre(666);  
+
+$repository->guardar($madre);
+$repository->cargar();
+
 
 
 
